be7a9c2222379d723b3e27230de165e64c5c206c
jarrellrn <jarrellrn@gmail.com>
2022-03-06 20:50:03 -0600

    added test data to the run method

--- index.js
+++ index.js
@@ -7,5 +7,7 @@ network.train([
   { input: [0,0,1], output: [0] },
   { input: [0,1,1], output: [0] },
   { input: [1,0,1], output: [1] },
-  { input: [1,1,1], output: [1] },
+  { input: [1,1,1], output: [1] }
 ]);
+
+const result = network.run([1,0,0]);
