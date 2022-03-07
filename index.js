fb7c5aa3372c3096b7b6c797e783055e1fa5e8be
jarrellrn <jarrellrn@gmail.com>
2022-03-07 11:56:59 -0600

    fleshed out the dataset a little more to provide more context

--- index.js
+++ index.js
@@ -2,14 +2,27 @@ const brain = require("brain.js");
 
 const network = new brain.NeuralNetwork();
 
+// network.train([
+//   { input: [0,0,0], output: [0] },
+//   { input: [0,0,1], output: [0] },
+//   { input: [0,1,1], output: [0] },
+//   { input: [1,0,1], output: [1] },
+//   { input: [1,1,1], output: [1] }
+// ]);
+
+
+// Think of the below as teams.
+// If the first team wins, the output is [0], if the second team wins, the output is [1].
 network.train([
-  { input: [0,0,0], output: [0] },
-  { input: [0,0,1], output: [0] },
-  { input: [0,1,1], output: [0] },
-  { input: [1,0,1], output: [1] },
-  { input: [1,1,1], output: [1] }
+  { input: [1,2], output: [1] }, // Team 2 wins
+  { input: [1,3], output: [1] }, // Team 3 wins
+  { input: [2,3], output: [0] }, // Team 2 wins
+  { input: [2,4], output: [1] }, // Team 4 wins
+  { input: [1,2], output: [0] }, // Team 1 wins
+  { input: [1,3], output: [0] }, // Team 1 wins
+  { input: [3,4], output: [0] }, // Team 3 wins
 ]);
 
-const result = network.run([1,1,0]);
+const result = network.run([1,4]);
 
-console.log(`Probability: ${result}`);
+console.log(`Probability: ${result}`); 
